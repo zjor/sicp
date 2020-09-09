32388eb4d46f4a449f18eb4588245f4e4b3737b3
Sergey Royz <zjor.se@gmail.com>
2020-09-09 10:54:04 +0200

    original sqrt

--- sicp-clojure/ex_1.7.clj
+++ sicp-clojure/ex_1.7.clj
@@ -18,14 +18,15 @@
 	(average guess (/ x guess))
 )
 
-(defn sqrt-iter [guess x]
-	(if (good-enough? guess x)
-		guess
-		(sqrt-iter (improve guess x) x)
-	)
-)
-
 (defn sqrt [x]
+
+	(defn sqrt-iter [guess]
+		(if (good-enough? guess x)
+			guess
+			(sqrt-iter (improve guess x) x)
+		)
+	)
+
 	(sqrt-iter 1.0 x)
 )
 
